89404f0b844d474dd54e0924382a3c828b993f92
Dimitrios VELEGRAKIS <dimitris.velegrakis@netcompany.com>
2025-12-16 10:22:31 +0200

    test

---
+++ android/app/src/main/java/com/tripmanager/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tripmanager.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tripmanager.app',
+  appName: 'Trip Manager',
+  webDir: 'build'
+};
+
+export default config;
